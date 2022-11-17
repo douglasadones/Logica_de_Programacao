4e70e12a34cf1413b07ac6fa253f592118ec938e
Douglas Adones <douglas_ad_@outlook.com>
2022-11-17 13:56:05 -0300

    novo_exercicio

--- Lista de exercício Nº 2 ESTRUTURAS DE SELEÇÃO/ex16 - Valor Estacionamento.py
+++ Lista de exercício Nº 2 ESTRUTURAS DE SELEÇÃO/ex16 - Valor Estacionamento.py
@@ -2,30 +2,25 @@
 horários de entrada e saída de um veículo (hora e minuto) e calcule o total a ser pago. Se o veículo permanece por mais de
 quatro horas no estacionamento, ele ganha uma lavagem grátis. O seu programa deve informar ao usuário se isto ocorrer."""
 
-horaentrada = int(input("Informe o horário de entrada do veículo (horas): "))
-minutoentrada = int(input("Informe o horário de entrada do veículo (minutos): "))
-horasaida = int(input("Informe o horário de saída do veículo (horas): "))
-minutosaida = int(input("Informe o horário de saída do veículo (minutos): "))
+entrada = int(input("Informe o horário de entrada do veículo (horas): ")) * 60
+entrada += int(input("Informe o horário de entrada do veículo (minutos): "))
+saida = int(input("Informe o horário de saída do veículo (horas): ")) * 60
+saida += int(input("Informe o horário de saída do veículo (minutos): "))
 lavagemgratis = False
 
-horasnoestacionamento = 0
+if saida >= entrada:
+    tempototalnoestacionamento = saida - entrada
+else:
+    tempototalnoestacionamento = abs((entrada - saida) - (24 * 60))
 
-if horasaida - horaentrada > 1:  # Verificação unicamente por horas
-    horasnoestacionamento = (horasaida - horaentrada) - 1
+if tempototalnoestacionamento > 240:
+    lavagemgratis = True
 
-if horaentrada > horasaida:  # Verificando o caso 00:00
-    horasnoestacionamento = (24 - (abs(horaentrada - horasaida))) - 1  # - 1 deixa a verificação da última hora para os minutos.
-
-if horaentrada != horasaida:  # Verificação por minutos
-    if minutoentrada <= minutosaida:
-        horasnoestacionamento += 1
-    if horasnoestacionamento >= 4:
-        lavagemgratis = True
-
-print(f"Horário de entrada: {horaentrada:0>2}:{minutoentrada:0>2}", " (Dia 1)" if horaentrada > horasaida else "")
-print(f"Horário de saída: {horasaida:0>2}:{minutosaida:0>2}", " (Dia 2)" if horaentrada > horasaida else "")
-print(f"Total de horas no estacionamento: {horasnoestacionamento}", "hora." if horasnoestacionamento == 1 else "horas.")
+print(f"Horário de entrada: {entrada // 60:0>2}:{entrada % 60:0>2}", "(Dia 1)" if saida < entrada else "")
+print(f"Horário de saída: {saida // 60:0>2}:{saida % 60:0>2}", "(Dia 2)" if saida < entrada else "")
+print(f"Total de horas no estacionamento: {tempototalnoestacionamento // 60:0>2}:{tempototalnoestacionamento % 60:0>2}")
+print(f"Você deve pagar um total de R${(tempototalnoestacionamento // 60) * 2.5:.2f}")
 if lavagemgratis:
     print("Você tem direito a uma lavagem grátis!")
 else:
-    print("Você ainda não possui direito a uma lavagem grátis. (Atinja 4 horas ou mais no estacionamento)")
+    print("Você ainda não possui direito a uma lavagem grátis. (Permaneça por mais de 4 horas no estacionamento)")
